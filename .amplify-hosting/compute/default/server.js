f91b4a36a89fbc574af845a7120ce4dcf5a0b959
Kaushik Subramaniam Gnanaskandan <kgnanask@amazon.com>
2024-03-27 11:21:37 -0700

    Initial commit

---
+++ .amplify-hosting/compute/default/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Cache-Control', 'private, no-cache, no-store, max-age=0, s-maxage=0, must-revalidate');
+  res.end();
+});
+
+server.listen(3000, '127.0.0.1');
